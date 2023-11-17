573fd168aec55da578fa0a950e849e687f387518
Be-bo <robfiker@gmail.com>
2023-11-16 19:37:09 -0800

    Added 2 base page components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Partnerships from './components/Partnerships';
+import AddPartnership from './components/AddPartnerships';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Partnerships />}></Route>
+          <Route path='/add-partnership' element={<AddPartnership />}></Route>
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
