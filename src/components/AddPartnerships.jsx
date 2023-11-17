dc43decd062c52e4f24a721d6badb6cd9c653546
Be-bo <robfiker@gmail.com>
2023-11-17 13:48:32 -0800

    Added base infrastructure for category selection

--- src/components/AddPartnerships.jsx
+++ src/components/AddPartnerships.jsx
@@ -4,10 +4,29 @@ import Leftbar from "./Leftbar/Leftbar";
 import PartnershipCard from "./PartnershipCard/PartnershipCard";
 import searchIcon from "../assets/icons/searchIcon.svg";
 import filterIcon from "../assets/icons/filterIcon.svg";
-
+import axios from 'axios';
 import "../components/AddPartnerships.scss";
+import { useState, useEffect } from "react";
 
 function AddPartnerships() {
+  const [categories, setCategories] = useState();
+  const [selectedCategory, setSelectedCategory] = useState();
+
+  useEffect(() => {
+    async function fetchCategories() {
+      const response = await axios.get("http://3.20.237.64:80/categories");
+      setCategories(response.data);
+    }
+    fetchCategories();
+  });
+
+  const handleCategoryChange = (event) => {
+    const { value } = event.target;
+    // TODO: pull appropriate merchants
+  }
+
+
+
   return (
     <div>
       <Topbar></Topbar>
@@ -29,11 +48,22 @@ function AddPartnerships() {
                   className="mainbody__searchinput"
                 ></input>
               </div>
-              <img
+              {/* <img
                 className="mainbody__filtericon"
                 alt="filter icon"
                 src={filterIcon}
-              ></img>
+              ></img> */}
+
+              <select
+                className="newInv__item-input"
+                value={selectedCategory}
+                name='category'
+                onChange={handleCategoryChange}
+              >
+                {categories?.map((category, index) => (
+                  <option key={category} value={category}>{category}</option>
+                ))}
+              </select>
             </div>
             <div className="mainbody__title">Suggested Merchant</div>
             <PartnershipCard />
