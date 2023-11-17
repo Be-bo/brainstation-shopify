8f02312b96ad219dec7eb92b437fd0eeb151516a
SopiaLee <sopialee06design@gmail.com>
2023-11-17 12:37:25 -0800

    add searchbar, card

--- src/components/AddPartnerships.jsx
+++ src/components/AddPartnerships.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-// import PartnershipCard from "./PartnershipCard/PartnershipCard";
 import Topbar from "./Topbar/Topbar";
 import Leftbar from "./Leftbar/Leftbar";
+import PartnershipCard from "./PartnershipCard/PartnershipCard";
+import searchIcon from "../assets/icons/searchIcon.svg";
+import filterIcon from "../assets/icons/filterIcon.svg";
 
 import "../components/AddPartnerships.scss";
 
@@ -13,7 +15,29 @@ function AddPartnerships() {
         <Leftbar></Leftbar>
         <div className="mainbody__content">
           <div className="mainbody__heading">Add New Partners</div>
-          <div className="mainbody__box"></div>
+          <div className="mainbody__box">
+            <div className="mainbody__searchbox">
+              <div className="mainbody__searchgroup">
+                <img
+                  className="mainbody__searchicon"
+                  alt="search icon"
+                  src={searchIcon}
+                ></img>
+                <input
+                  type="search"
+                  placeholder="Search"
+                  className="mainbody__searchinput"
+                ></input>
+              </div>
+              <img
+                className="mainbody__filtericon"
+                alt="filter icon"
+                src={filterIcon}
+              ></img>
+            </div>
+            <div className="mainbody__title">Suggested Merchant</div>
+            <PartnershipCard />
+          </div>
         </div>
       </div>
     </div>
