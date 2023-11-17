912c22170635c9038cb1532f5accffd1bcc89142
Be-bo <robfiker@gmail.com>
2023-11-16 21:51:30 -0800

    Added rfce to both core component pages

---
+++ src/components/AddPartnerships.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddPartnerships() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AddPartnerships
---
+++ src/components/Partnerships.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Partnerships() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Partnerships
